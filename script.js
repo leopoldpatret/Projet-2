238532cd3157040a86767e0f40107337d4fda03e
leopoldpatret <46608217+leopoldpatret@users.noreply.github.com>
2019-03-19 16:40:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,36 @@
 //Équipier 1 : Pierre-Olivier Tremblay - 20049076
 //Équipier 2 : Leopold Patret - 20137744
+
+		//// DIFFERENTES FONCTIONS ////
+var validateUsername= function (value){
+
+
+}
+var validatePassword= function (value){
+
+
+} 
+var updateHeader = function (username){
+
+
+}
+var searchMovies = function (movies,searchValue){
+
+
+}
+var sortMovies = function (movies,isAscending){
+
+
+}
+var populateSelect = function( movies ) {
+
+
+}
+var filterMovies = function(movies,genreFilter){
+
+
+}
+var displayMovie = function(movie){
+
+
+}
