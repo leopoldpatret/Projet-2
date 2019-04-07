6bee39efbd6e09cc2ca1ef7ea671c90ef6a38f69
leopoldpatret <46608217+leopoldpatret@users.noreply.github.com>
2019-04-07 17:52:20 -0400

    Update script.js

--- _assignment_/script.js
+++ _assignment_/script.js
@@ -160,3 +160,36 @@ var filterMovies = function(movies, genreFilter) {
 var displayMovie = function(movie) {
   //appelée lorsque l'application à besoin d'afficher un film. Celle-ci doit retourner un objet JavaScript particulier.
 };
+/*
+<div class="c-main_item_card">
+<div class="c-main_item_cover -red">
+<p class="c-main_item_preview">S</p>
+<div class="c-main_item_details">
+<h3 class="c-main_item_header">Title :</h3>
+<p class="c-main_item_text">Sample</p>
+<h3 class="c-main_item_header">Genre :</h3>
+<p class="c-main_item_text">Action, Funny, Drama</p>
+<h3 class="c-main_item_header">Synopsis :</h3>
+<p class="c-main_item_text">
+Some amazing content full of interesting things and cool text that will deeply affect your life.
+</p>
+<h3 class="c-main_item_header">
+        Length :
+        <span class="c-main_item_span">2h05min</span>
+        </h3><h3 class="c-main_item_header">
+        Language :
+        <span class="c-main_item_span">EN</span>
+        </h3><h3 class="c-main_item_header">
+        Rating :
+        <span class="c-main_item_span">9.7 / 10</span>
+        </h3>
+        </div>
+        </div>
+        <h2 title="Sample" class="c-main_item_title">Sample</h2>
+        </div>
+
+
+         tout sa c'est le code html que le site utilise pour afficher l'échantillon qui est donnée dans la consigne 
+         on vas donc devoir utiliser toute les classe qui son la et les remplir pour chaque film je sais pas si on sépare chaque 
+         variable (genre, duree, rating, ...) ou si on essaye de faire un fonction dans la fonction qui va etre utilisé pour l'affichage.
+*/
